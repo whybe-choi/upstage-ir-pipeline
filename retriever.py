e6f66134b94dcf78f4a61783188b2203a5e41af4
whybe-choi <whybe.choi@gmail.com>
2024-06-24 03:21:53 +0000

    feat: Define load_ensemble_retriever func

--- retriever.py
+++ retriever.py
@@ -4,6 +4,7 @@ from langchain.docstore.document import Document
 from langchain.retrievers import EnsembleRetriever
 from langchain_community.retrievers import BM25Retriever
 from langchain_community.vectorstores import Chroma
+
 from vectorstore import load_embed_model_from_local
 from pprint import pprint
 
@@ -31,7 +32,7 @@ def load_sparse_retriever(data_path, k):
         )
         documents.append(document)
 
-    sparse_retriever = BM25Retriever.from_documents(documents, preprocess_func=kiwi_tokenize, k=3)
+    sparse_retriever = BM25Retriever.from_documents(documents, preprocess_func=kiwi_tokenize, k=k)
     return sparse_retriever
 
 def load_dense_retriever(persist_path, k):
@@ -48,32 +49,27 @@ def load_dense_retriever(persist_path, k):
 
     return dense_retriever
 
+def load_ensemble_retriever(dense_retriever, sparse_retriever, dense_weight, sparse_weight, k):
+    """
+    Load the ensemble retriever.
+    """
+    ensemble_retriever = EnsembleRetriever(
+        retrievers=[dense_retriever, sparse_retriever],
+        weight=[dense_weight, sparse_weight],
+        search_kwagrgs={"k": k},
+    )
+    return ensemble_retriever
+
+
 def print_retrieved_docs(retriever):
 
-    retrieved_1 = retriever.get_relevant_documents("피임을 위한 약") 
-    # ["99a07643-8479-4d34-9de8-68627854f458", "5eef496e-a26d-44aa-bd84-87152c7ed771", "e5fdf1a0-337d-4032-abaa-19a6bcdbf304"]
-    retrieved_2 = retriever.get_relevant_documents("금성에서 달이 어떻게 보일까?") 
-    # ["35c5dcc7-4720-4318-901e-770105ae63fd", "59a8259f-4a39-4ab6-ad3f-2e4161ad458d", "464ace62-ddf2-423d-a5d7-2f17e6785c8e"]
-    retrieved_3 = retriever.get_relevant_documents("소프트웨어 개발과 편향")
+    retrieved = retriever.get_relevant_documents("피임을 위한 약") 
 
-    print("Query1: 피임을 위한 약")
-    for idx, retrieved_doc in enumerate(retrieved_1):
+    print("Query: 피임을 위한 약")
+    for idx, retrieved_doc in enumerate(retrieved):
         print("="*30, f"문서 {idx+1}", "="*30)
         pprint(retrieved_doc)
         print("="*68)
-    print()
-    print("Query2: 금성에서 달이 어떻게 보일까?")
-    for idx, retrieved_doc in enumerate(retrieved_2):
-        print("="*30, f"문서 {idx+1}", "="*30)
-        pprint(retrieved_doc)
-        print("="*68)
-    print()
-    print("Query3: 소프트웨어 개발과 편향")
-    for idx, retrieved_doc in enumerate(retrieved_3):
-        print("="*30, f"문서 {idx+1}", "="*30)
-        pprint(retrieved_doc)
-        print("="*68)
-
 
 
 if __name__ == "__main__":
